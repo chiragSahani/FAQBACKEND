5eff531d527a3083b8d6446e6709a90c6274c488
Chirag <145746922+chiragSahani@users.noreply.github.com>
2025-02-01 19:35:33 +0530

    Update faq.js

--- models/faq.js
+++ models/faq.js
@@ -1,18 +1,75 @@
-import { Router } from "express";
-import {
-  getFAQs,
-  getOneFAQ,
-  createFAQ,
-  updateFAQ,
-  deleteFAQ,
-} from "../controllers/faq.controller.js";
+import mongoose from "mongoose";
+import translate from "google-translate-api-x";
 
-const router = Router();
+import { languages } from "../utils/language.js ";
+import { ParseDOM } from "../utils/parseDom.js";
 
-router.get("/", getFAQs);
-router.get("/:id", getOneFAQ);
-router.post("/", createFAQ);
-router.put("/:id", updateFAQ);
-router.delete("/:id", deleteFAQ);
+const faqSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+    translations: {
+      hi: { question: String, answer: String }, // Hindi
+      gu: { question: String, answer: String }, // Gujarati
+      mr: { question: String, answer: String }, // Marathi
+      bn: { question: String, answer: String }, // Bengali
+    },
+  },
+  { timestamps: true }
+);
 
-export default router;
+faqSchema.pre("save", async function (next) {
+  if (this.isModified("question")) {
+    const translationPromises = languages.map(async (lang) => {
+      const response = await translate(this.question, { to: lang });
+      return { lang, text: response.text };
+    });
+    const translations = await Promise.all(translationPromises);
+    translations.forEach(({ lang, text }) => {
+      this.translations[lang].question = text;
+    });
+  }
+
+  if (this.isModified("answer")) {
+    await ParseDOM(this);
+  }
+
+  next();
+});
+
+faqSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate();
+
+  if (update.question) {
+    const translationPromises = languages.map(async (lang) => {
+      const response = await translate(update.question, { to: lang });
+      return { lang, text: response.text };
+    });
+    const translations = await Promise.all(translationPromises);
+    translations.forEach(({ lang, text }) => {
+      update[`translations.${lang}.question`] = text;
+    });
+  }
+
+  if (update.answer) {
+   await ParseDOM(update);
+  }
+
+  this.setUpdate(update);
+  next();
+});
+
+faqSchema.methods.getTranslation = function (lang) {
+  return (
+    this.translations[lang] || { question: this.question, answer: this.answer }
+  );
+};
+
+export const FAQ = mongoose.model("faq", faqSchema);
