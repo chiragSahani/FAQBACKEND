abcfc801647e7ee13ec7049400139aab8a33b0f5
Chirag <145746922+chiragSahani@users.noreply.github.com>
2025-02-01 19:42:33 +0530

    Update apiER.js

--- utils/apiER.js
+++ utils/apiER.js
@@ -1,20 +1,18 @@
 class APIError extends Error {
-    constructor(
-      statusCode,
-      message = "Something went wrong",
-      type = "UNKNOWN",
-      errors = [],
-    ) {
-      super(message);
-      this.statusCode = statusCode;
-      this.type = type;
-      this.success = false;
-      this.errors = errors;
-  
-      if (stack) {
-      Error.captureStackTrace(this, this.constructor);
-    }
-    }
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    type = "UNKNOWN",
+    errors = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.type = type;
+    this.success = false;
+    this.errors = errors;
+
+    Error.captureStackTrace(this, this.constructor);
   }
-  
-  export default APIError;
+}
+
+export default APIError;
